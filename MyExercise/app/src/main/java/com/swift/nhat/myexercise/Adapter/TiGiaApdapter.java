8ad0f81d8c0f2c4279a83c5b893f2d8bc00784ca
Cao Khanh Nhat <51302751@hcmut.edu.vn>
2017-03-14 19:31:12 +0700

    App Ti Gia Tien Te

---
+++ MyExercise/app/src/main/java/com/swift/nhat/myexercise/Adapter/TiGiaApdapter.java
@@ -0,0 +1,8 @@
+package com.swift.nhat.myexercise.Adapter;
+
+/**
+ * Created by nhat on 3/14/17.
+ */
+
+public class TiGiaApdapter {
+}
---
+++ MyExercise/app/src/main/java/com/swift/nhat/myexercise/Model/TiGia.java
@@ -0,0 +1,8 @@
+package com.swift.nhat.myexercise.Model;
+
+/**
+ * Created by nhat on 3/14/17.
+ */
+
+public class TiGia {
+}
